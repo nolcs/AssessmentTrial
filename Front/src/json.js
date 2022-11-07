11adea3c79273ed693cf9ae4c2baf1f37691c724
Nolasco <nolasco.jimenez@solera.com>
2022-11-07 11:47:44 +0100

    Config

--- Front/src/json.js
+++ Front/src/json.js
@@ -1,86 +1,66 @@
 export const json = {
-  "title": "American History",
-  "showProgressBar": "bottom",
-  "showTimerPanel": "top",
-  "maxTimeToFinishPage": 10,
-  "maxTimeToFinish": 25,
-  "firstPageIsStarted": true,
-  "startSurveyText": "Start Quiz",
-  "pages": [
+  title: "Blockchain afinity",
+  showProgressBar: "bottom",
+  firstPageIsStarted: true,
+  startSurveyText: "Start Quiz",
+  pages: [
     {
-      "elements": [
+      elements: [
         {
-          "type": "html",
-          "html": "You are about to start a quiz on American history. <br>You will have 10 seconds for every question and 25 seconds to end the quiz.<br>Enter your name below and click <b>Start Quiz</b> to begin."
+          type: "html",
+          html:
+            "You are about to start a questionnaire on Business Strategy. <br>Enter your email below and click <b>Start Quiz</b> to begin.",
         },
         {
-          "type": "text",
-          "name": "username",
-          "titleLocation": "hidden",
-          "isRequired": true
-        }
-      ]
+          type: "text",
+          name: "email",
+          titleLocation: "hidden",
+          isRequired: true,
+        },
+      ],
     },
     {
-      "elements": [
+      elements: [
         {
-          "type": "radiogroup",
-          "name": "civilwar",
-          "title": "When was the American Civil War?",
-          "choices": [
-            "1796-1803",
-            "1810-1814",
-            "1861-1865",
-            "1939-1945"
-          ],
-          "correctAnswer": "1861-1865"
-        }
-      ]
+          type: "radiogroup",
+          name: "civilwar",
+          title: "When was the American Civil War?",
+          choices: ["1796-1803", "1810-1814", "1861-1865", "1939-1945"],
+        },
+      ],
     },
     {
-      "elements": [
+      elements: [
         {
-          "type": "radiogroup",
-          "name": "libertyordeath",
-          "title": "Whose quote is this: \"Give me liberty, or give me death\"?",
-          "choicesOrder": "random",
-          "choices": [
+          type: "radiogroup",
+          name: "libertyordeath",
+          title: 'Whose quote is this: "Give me liberty, or give me death"?',
+          choicesOrder: "random",
+          choices: [
             "John Hancock",
             "James Madison",
             "Patrick Henry",
-            "Samuel Adams"
+            "Samuel Adams",
           ],
-          "correctAnswer": "Patrick Henry"
-        }
-      ]
+        },
+      ],
     },
     {
-      "elements": [
+      elements: [
         {
-          "type": "radiogroup",
-          "name": "magnacarta",
-          "title": "What is Magna Carta?",
-          "choicesOrder": "random",
-          "choices": [
+          type: "radiogroup",
+          name: "magnacarta",
+          title: "What is Magna Carta?",
+          choicesOrder: "random",
+          choices: [
             "The foundation of the British parliamentary system",
             "The Great Seal of the monarchs of England",
             "The French Declaration of the Rights of Man",
-            "The charter signed by the Pilgrims on the Mayflower"
+            "The charter signed by the Pilgrims on the Mayflower",
           ],
-          "correctAnswer": "The foundation of the British parliamentary system"
-        }
-      ]
-    }
-  ],
-  "completedHtml": "<h4>You got <b>{correctAnswers}</b> out of <b>{questionCount}</b> correct answers.</h4>",
-  "completedHtmlOnCondition": [
-    {
-      "expression": "{correctAnswers} == 0",
-      "html": "<h4>Unfortunately, none of your answers is correct. Please try again.</h4>"
+        },
+      ],
     },
-    {
-      "expression": "{correctAnswers} == {questionCount}",
-      "html": "<h4>Congratulations! You answered all the questions correctly!</h4>"
-    }
-  ]
+  ],
+  completedHtml: "<h4>Thank you for your answers.</h4>",
 };
