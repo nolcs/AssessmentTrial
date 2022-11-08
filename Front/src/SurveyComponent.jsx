0741f060a09ab540a1d4132ec7f21639c3602215
Nolasco <nolasco.jimenez@solera.com>
2022-11-08 11:26:32 +0100

    frontend first version

--- Front/src/SurveyComponent.jsx
+++ Front/src/SurveyComponent.jsx
@@ -5,14 +5,32 @@ import { Survey } from "survey-react-ui";
 
 import "survey-core/defaultV2.css";
 import "./index.css";
-import { json } from "./json"
+import { json } from "./json";
 
 StylesManager.applyTheme("defaultV2");
 
 function SurveyComponent() {
-    const survey = new Model(json);
+  const survey = new Model(json);
 
-    return (<Survey model={survey} />);
+  survey.onComplete.add(function(sender, options) {
+    options.showDataSaving();
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "your/server/url");
+    xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    xhr.onload = xhr.onerror = function() {
+      if (xhr.status == 200) {
+        options.showDataSavingSuccess();
+        // options.showDataSavingClear();
+      } else {
+        options.showDataSavingError();
+      }
+    };
+
+    console.log(sender.data);
+    xhr.send(JSON.stringify(sender.data));
+  });
+
+  return <Survey model={survey} />;
 }
 
 export default SurveyComponent;
--- Front/src/json.js
+++ Front/src/json.js
@@ -1,66 +1,105 @@
+const answersasync = (body) => {
+  const answersrequestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  };
+
+  let res = fetch("http://localhost:8081/questions", answersrequestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data !== undefined) {
+        questions.push(data);
+      }
+    });
+};
+
+const questions = [
+  {
+    id: 1,
+    question: "viva",
+  },
+  {
+    id: 2,
+    question: "er",
+  },
+  {
+    id: 3,
+    question: "beti",
+  },
+];
+
+let processedQuestions = [
+  {
+    elements: [
+      {
+        type: "html",
+        html:
+          "You are about to start a questionnaire on Business Strategy. <br>Enter your email below and click <b>Start Quiz</b> to begin.",
+      },
+      {
+        type: "text",
+        name: "email",
+        titleLocation: "hidden",
+        isRequired: true,
+        validators: [{ type: "email", text: "Value must be an email" }],
+      },
+    ],
+  },
+];
+
+questions.forEach((item) => {
+  const processedElement = {
+    elements: [
+      {
+        type: "text",
+        name: "" + item.id + "",
+        title: "" + item.question + "",
+        isRequired: true,
+      },
+    ],
+  };
+
+  processedQuestions.push(processedElement);
+});
+
+// console.log(processedQuestions);
+
 export const json = {
   title: "Blockchain afinity",
   showProgressBar: "bottom",
   firstPageIsStarted: true,
   startSurveyText: "Start Quiz",
-  pages: [
-    {
-      elements: [
-        {
-          type: "html",
-          html:
-            "You are about to start a questionnaire on Business Strategy. <br>Enter your email below and click <b>Start Quiz</b> to begin.",
-        },
-        {
-          type: "text",
-          name: "email",
-          titleLocation: "hidden",
-          isRequired: true,
-        },
-      ],
-    },
-    {
-      elements: [
-        {
-          type: "radiogroup",
-          name: "civilwar",
-          title: "When was the American Civil War?",
-          choices: ["1796-1803", "1810-1814", "1861-1865", "1939-1945"],
-        },
-      ],
-    },
-    {
-      elements: [
-        {
-          type: "radiogroup",
-          name: "libertyordeath",
-          title: 'Whose quote is this: "Give me liberty, or give me death"?',
-          choicesOrder: "random",
-          choices: [
-            "John Hancock",
-            "James Madison",
-            "Patrick Henry",
-            "Samuel Adams",
-          ],
-        },
-      ],
-    },
-    {
-      elements: [
-        {
-          type: "radiogroup",
-          name: "magnacarta",
-          title: "What is Magna Carta?",
-          choicesOrder: "random",
-          choices: [
-            "The foundation of the British parliamentary system",
-            "The Great Seal of the monarchs of England",
-            "The French Declaration of the Rights of Man",
-            "The charter signed by the Pilgrims on the Mayflower",
-          ],
-        },
-      ],
-    },
-  ],
-  completedHtml: "<h4>Thank you for your answers.</h4>",
+  pages: processedQuestions,
+  completedHtml: "<h4>Thank you for your answers</h4>",
+  awnsersSubmitHandler,
+};
+// console.log(json);
+
+let answerArray = [];
+
+const awnsersSubmitHandler = (anwserData) => {
+  console.log(anwserData);
+  answerArray = anwserData;
+  sendAnwsersHandler();
+};
+
+const sendAnwsersHandler = () => {
+  answerArray.map((item) => {
+    let anwserBody = {
+      questionid: item.questionid,
+      anwser: item.anwser,
+    };
+    const anwserrequestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(anwserBody),
+    };
+    console.log(anwserBody);
+    let res = fetch("http://localhost:8084/anwsers", anwserrequestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  });
 };
