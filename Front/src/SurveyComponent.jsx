b9f51c18412fbeb9f734b604be26812c1ee7d9ff
Nolasco <nolasco.jimenez@solera.com>
2022-11-08 14:43:24 +0100

    fixes

--- Front/src/SurveyComponent.jsx
+++ Front/src/SurveyComponent.jsx
@@ -11,24 +11,53 @@ StylesManager.applyTheme("defaultV2");
 
 function SurveyComponent() {
   const survey = new Model(json);
-
-  survey.onComplete.add(function(sender, options) {
-    options.showDataSaving();
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "your/server/url");
-    xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-    xhr.onload = xhr.onerror = function() {
-      if (xhr.status == 200) {
-        options.showDataSavingSuccess();
-        // options.showDataSavingClear();
-      } else {
-        options.showDataSavingError();
-      }
-    };
-
-    console.log(sender.data);
-    xhr.send(JSON.stringify(sender.data));
+  let user;
+  survey.onValueChanged.add((survey, { name }) => {
+    // console.log(`The ${name} question value has changed to ${value}.`);
+    saveQuestion(survey, name);
   });
+
+  function saveQuestion(survey, name) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", " localhost:8085/users/createUser");
+    xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    let question = survey.getQuestionByName(name);
+    let answerObj = new Object();
+    if (name == "email") {
+      user = question.value;
+      answerObj.email = question.value;
+    } else {
+      let questionObj = new Object();
+      let userObj = new Object();
+      questionObj.id = question.name;
+      answerObj.question = questionObj;
+      answerObj.answer = question.value;
+      // userObj.id = user;
+      // answerObj.user = userObj; GET userId
+    }
+
+    console.log(answerObj);
+    xhr.send(answerObj);
+  }
+
+  //   survey.onComplete.add(function(sender, options) {
+  //     options.showDataSaving();
+  //     const xhr = new XMLHttpRequest();
+  //     xhr.open("POST", "your/server/url");
+  //     xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+  //     xhr.onload = xhr.onerror = function() {
+  //       if (xhr.status == 200) {
+  //         options.showDataSavingSuccess();
+  //         // options.showDataSavingClear();
+  //       } else {
+  //         options.showDataSavingError();
+  //       }
+  //     };
+
+  //     console.log(sender.data);
+  //     console.log(JSON.stringify(sender.data));
+  //     xhr.send(JSON.stringify(sender.data));
+  //   });
 
   return <Survey model={survey} />;
 }
